f0725f26a46353b0324c7691950f6873a64f2b4c
axilirate <56825186+axilirate@users.noreply.github.com>
2021-03-21 19:12:13 +0200

    Tarot cards setup.

--- Classes/Actions/PrayAction.gd
+++ Classes/Actions/PrayAction.gd
@@ -6,12 +6,8 @@ const TOOLTIP := "pray"
 
 func _ready():
 
-	var _minutes_passed = 1
-	var _energy_cost = 0.001
 	var _main_story = str(executer.character_name) + " have prayed"
-	
 	var emit_story_telling = emit_story_telling(_main_story)
-	calculate_turn(_energy_cost, _minutes_passed)
 	
 	yield(emit_story_telling, "completed")
 	queue_free()
