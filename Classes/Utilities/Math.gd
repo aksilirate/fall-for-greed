e3fc332f78663be8e04272e24b7351e05a318b2f
axilirate <56825186+axilirate@users.noreply.github.com>
2021-04-03 14:51:53 +0300

    array map function

--- Classes/Utilities/Math.gd
+++ Classes/Utilities/Math.gd
@@ -18,3 +18,15 @@ class_name Math
 static func range_with_peak(max_index: float, index: float):
 	var to_sqr =  ( (2/-max_index) * index) + 1 
 	return (-1 * (to_sqr * to_sqr) )   +  1
+
+
+
+static func create_map(width, height):
+	var map = []
+
+	for x in range(width):
+		var col = []
+		col.resize(height)
+		map.append(col)
+
+	return map
