bba631a120ed3ca7432ebcaecd0debc6f38b55f8
axilirate <56825186+axilirate@users.noreply.github.com>
2021-04-07 16:22:46 +0300

    useless

---
+++ Areas/Hills/Zones/ToMountains.gd
@@ -0,0 +1,17 @@
+extends Zone
+class_name ToMountains
+
+
+const HISTORY = "Mountains."
+
+
+const TEXTURES = [
+	"res://Textures/Areas/Hills/Zones/To Mountains.png"
+]
+
+
+
+const WEST_ACTION = WaitAction
+const LEFT_ACTION = WalkAction
+const RIGHT_ACTION = SleepAction
+const EAST_ACTION = SearchAction
